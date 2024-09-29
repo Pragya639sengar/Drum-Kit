8d2b40f2bbb3cb6b237079eae4c50b57e3bc1dd4
Pragya639sengar <81747285+Pragya639sengar@users.noreply.github.com>
2024-09-29 16:18:23 +0530

    Updated myscript.js
    "\sounds" folder was not here, I directly imported the sound files so only the names of the sound files have been mentioned.

--- myscript.js
+++ myscript.js
@@ -15,31 +15,31 @@ document.addEventListener("keydown", function(event){
 function makeSound(key){
   switch (key) {
     case "c":
-      var audio1 = new Audio("sounds/crash.mp3");
+      var audio1 = new Audio("crash.mp3");
       audio1.play();
       break;
     case "k":
-      var audio2 = new Audio("sounds/kick-bass.mp3");
+      var audio2 = new Audio("kick-bass.mp3");
       audio2.play();
       break;
     case "s":
-      var audio3 = new Audio("sounds/snare.mp3");
+      var audio3 = new Audio("snare.mp3");
       audio3.play();
       break;
     case "t":
-      var audio4 = new Audio("sounds/tom-1.mp3");
+      var audio4 = new Audio("tom-1.mp3");
       audio4.play();
       break;
     case "b":
-      var audio5 = new Audio("sounds/tom-2.mp3");
+      var audio5 = new Audio("tom-2.mp3");
       audio5.play();
       break;
     case "p":
-      var audio6 = new Audio("sounds/tom-3.mp3");
+      var audio6 = new Audio("tom-3.mp3");
       audio6.play();
       break;
     case "v":
-      var audio7 = new Audio("sounds/tom-4.mp3");
+      var audio7 = new Audio("tom-4.mp3");
       audio7.play();
       break;
     default:
