c860c508bf689e59f0b67eb57b3fc618ed4bdae0
Pragya639sengar <81747285+Pragya639sengar@users.noreply.github.com>
2024-09-29 15:40:22 +0530

    Add files via upload
    Created a mini drum kit.

---
+++ myscript.js
@@ -0,0 +1,48 @@
+//Detecting screen button press
+var numberOfDrumbtn = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumbtn; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var btnInnerhtml = this.innerHTML;
+    makeSound(btnInnerhtml);
+  });
+}
+
+//Detecting keyboard press
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "c":
+      var audio1 = new Audio("sounds/crash.mp3");
+      audio1.play();
+      break;
+    case "k":
+      var audio2 = new Audio("sounds/kick-bass.mp3");
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio("sounds/snare.mp3");
+      audio3.play();
+      break;
+    case "t":
+      var audio4 = new Audio("sounds/tom-1.mp3");
+      audio4.play();
+      break;
+    case "b":
+      var audio5 = new Audio("sounds/tom-2.mp3");
+      audio5.play();
+      break;
+    case "p":
+      var audio6 = new Audio("sounds/tom-3.mp3");
+      audio6.play();
+      break;
+    case "v":
+      var audio7 = new Audio("sounds/tom-4.mp3");
+      audio7.play();
+      break;
+    default:
+      alert("random key pressed!");
+  }
+}
